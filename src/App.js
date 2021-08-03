f84617e3955bc98ef364db23893057c8db2e5b87
Abdul-cmd <Abdussamadahmad562@gmail.com>
2021-08-03 14:59:20 -0700

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import DocTileOne from './components/DocTileOne';
+import DocTileTwo from './components/DocTitleTwo';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <DocTileOne />
+     <DocTileTwo />
+             
+    </div>
+  );
+}
+
+export default App;
